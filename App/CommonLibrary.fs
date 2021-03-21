f591c13d0647d8066fd7a89172bead8cb6da69a6
David Trång <david.a.trang@gmail.com>
2021-03-21 18:24:08 +0100

    Remove unused code

--- App/CommonLibrary.fs
+++ App/CommonLibrary.fs
@@ -4,28 +4,6 @@ type ValidationError = ValidationError of string
 
 [<RequireQualifiedAccess>]
 module Result = 
-  let isError x =
-    match x with
-    | Ok _ -> false
-    | Error _ -> true
-
-  let getError x =
-    match x with
-    | Ok _ -> None
-    | Error e -> Some e
-
-  let foldError f (results: Result<'a, 'b> seq) =
-    let errors = 
-      results 
-      |> Seq.filter isError
-      |> Seq.reduce f
-
-    let oks = 
-      results
-      |> Seq.filter (isError >> not)
-  
-    Seq.append oks [errors]
-
   let apply fResult xResult =
     match fResult, xResult with
     | Ok f, Ok x -> Ok (f x)
